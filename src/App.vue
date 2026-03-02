ac571972edc40c694a2457207df3f2402766588a
samg <sam.gallloway303@gmail.com>
2026-03-01 19:35:55 -0500

    Update profile links with real URLs

--- src/App.vue
+++ src/App.vue
@@ -34,23 +34,23 @@ const links = [
   },
   {
     label: 'LinkedIn',
-    url: '#',
+    url: 'https://www.linkedin.com/in/samuel-galloway-98bbb5162/',
     icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>',
   },
   {
     label: 'YouTube',
-    url: '#',
+    url: 'https://www.youtube.com/@sambgalloway',
     icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>',
   },
   {
     label: 'Bluesky',
-    url: '#',
+    url: 'https://bsky.app/profile/sambg.bsky.social',
     icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.596 6.449.76 2.784 3.471 3.7 5.998 3.435-.363.065-.728.108-1.094.126-2.07.1-4.267.96-1.595 3.386C6.45 19.558 8.70 20.935 10.54 17.58c.428-.783.793-1.708 1.46-1.708.667 0 1.032.925 1.46 1.708 1.84 3.355 4.09 1.978 6.635-.416 2.672-2.426.475-3.287-1.595-3.386a10.683 10.683 0 0 1-1.094-.126c2.527.265 5.239-.65 5.998-3.435C23.622 9.418 24 4.458 24 3.768c0-.69-.139-1.861-.902-2.203-.659-.3-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8z"/></svg>',
   },
   {
-    label: 'Employer',
-    url: '#',
-    icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"/><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"/></svg>',
+    label: 'Orases',
+    url: 'https://orases.com/',
+    icon: '<img src="https://orases.com/wp-content/uploads/2024/11/orases-logo-small-white.png" alt="Orases" width="20" height="20" style="object-fit: contain" />',
   },
 ]
 </script>
